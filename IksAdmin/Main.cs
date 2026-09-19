d53e9704fbeb422fe4bdc0e709fd3b92a0c2d985
Dancebunny98 <37336183+dancebunny98@users.noreply.github.com>
2026-09-19 05:28:05 +0300

    Update Main.cs

--- IksAdmin/Main.cs
+++ IksAdmin/Main.cs
@@ -172,11 +172,11 @@ public class Main : BasePlugin
 
                     if (ban != null)
                     {
-                        AdminApi.DisconnectPlayer(
+                        DisconnectPlayer(
                             player,
                             ban.Reason,
                             customMessageTemplate: Localizer["HTML.AdvancedBanMessage"],
-                            disconnectionReason: CounterStrikeSharp.API.ValveConstants.Protobuf.NetworkDisconnectionReason.NETWORK_DISCONNECT_STEAM_BANNED,
+                            disconnectionReason: NetworkDisconnectionReason.NETWORK_DISCONNECT_STEAM_BANNED,
                             admin: ban.Admin,
                             disconnectedBy: "ban"
                         );
